7a31fb5b1532702706c7b57d4dcd66fbd567d584
Webolim <aperture22@gmail.com>
2026-02-26 16:04:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ export default defineConfig({
   // TODO: Set this to your repository name if deploying to https://webolim.github.io/Valmiki-Ramayana-Conference-2026/
   // Example: base: '/my-project/',
   base: '/Valmiki-Ramayana-Conference-2026/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        print: 'print.html',
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
